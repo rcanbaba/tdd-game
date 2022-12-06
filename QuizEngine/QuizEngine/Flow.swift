85fdab9f772ea8bf1fd9f1a90bc92a60c9c90fdd
rcanbaba <rcanbabaoglu@gmail.com>
2022-12-06 20:27:26 +0300

    QuizEngine started, initial project settings..

---
+++ QuizEngine/QuizEngine/Flow.swift
@@ -0,0 +1,8 @@
+//
+//  Flow.swift
+//  QuizEngine
+//
+//  Created by Can Babaoğlu on 6.12.2022.
+//
+
+import Foundation
---
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -0,0 +1,15 @@
+//
+//  Flow.swift
+//  QuizEngineTests
+//
+//  Created by Can Babaoğlu on 6.12.2022.
+//
+
+import XCTest
+
+@testable import QuizEngine
+
+final class FlowTest: XCTestCase {
+        
+    
+}
