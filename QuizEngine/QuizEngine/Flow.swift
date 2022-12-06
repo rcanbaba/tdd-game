cbd1a86755f412c30bb755810fb0af7d5408da89
rcanbaba <rcanbabaoglu@gmail.com>
2022-12-06 20:50:32 +0300

    tdd implementation started.

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol Router {
+    func routeTo(question: String)
+}
+
+class Flow {
+    let router: Router
+    let questions: [String]
+    
+    init(questions: [String], router: Router) {
+        self.questions = questions
+        self.router = router
+    }
+    
+    func start() {
+        if !questions.isEmpty {
+            router.routeTo(question: "Q1")
+        }
+    }
+}
--- QuizEngine/QuizEngineTests/FlowTest.swift
+++ QuizEngine/QuizEngineTests/FlowTest.swift
@@ -10,6 +10,40 @@ import XCTest
 @testable import QuizEngine
 
 final class FlowTest: XCTestCase {
+    func test_start_withNoQuestions_doesNotRouteToQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: [], router: router)
         
+        sut.start()
+        
+        XCTAssertEqual(router.routedQuestionCount, 0)
+    }
     
+    func test_start_withOneQuestion_routeToQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1"], router: router)
+
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestionCount, 1)
+    }
+    
+    func test_start_withOneQuestion_routesToCorrectQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1"], router: router)
+
+        sut.start()
+
+        XCTAssertEqual(router.routedQuestion, "Q1")
+    }
+    
+    class RouterSpy: Router {
+        var routedQuestionCount: Int = 0
+        var routedQuestion: String? = nil
+        
+        func routeTo(question: String) {
+            routedQuestionCount += 1
+            routedQuestion = question
+        }
+    }
 }
